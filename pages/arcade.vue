c2cef4b69c2c96ca83555fe76a401bc707a71780
Manas Malla <manasmalla.dev@gmail.com>
2024-10-29 05:21:46 +0000

    Added arcade page

---
+++ pages/arcade.vue
@@ -0,0 +1,41 @@
+<template>
+    <NuxtLayout name="default">
+        <v-container fluid class="mt-5">
+            <h1>Arcade</h1>
+            <p style="width: 40ch;">Felt something missing? Well, you are at the right spot. Grab your thinking hats and put them on as we're going to play and compete to learn and earn, all while having fun.</p>
+            </v-container>
+        </NuxtLayout>
+        </template>
+
+        
+<script setup>
+const { faqData, mainData } = useJSONData();
+
+definePageMeta({
+  layout: false,
+});
+
+useSeoMeta({
+  contentType: "text/html; charset=utf-8",
+  title: "Arcade - " + mainData.eventInfo.name + " | " + mainData.communityName,
+  description: mainData.eventInfo.description.short,
+  keywords: mainData.seo.keywords,
+  ogLocale:'en_US',
+  author: "Manas Malla",
+  creator: "Manas Malla",
+  viewport: "width=device-width, initial-scale=1.0",
+  ogTitle: "Arcade - " + mainData.eventInfo.name + " | " + mainData.communityName,
+  ogDescription: mainData.eventInfo.description.short,
+  ogImage: `${mainData.seo.hostUrl}/thumbnail.png?auto=format&fit=crop&frame=1&h=512&w=1024`,
+  ogUrl: mainData.seo.hostUrl,
+  ogType: "website",
+  twitterTitle:
+    "Arcade - " + mainData.eventInfo.name + " | " + mainData.communityName,
+  twitterDescription: mainData.eventInfo.description.short,
+  twitterImage: `${mainData.seo.hostUrl}thumbnail.png?auto=format&fit=crop&frame=1&h=512&w=1024`,
+  twitterCard: "summary_large_image",
+});
+</script>
+
+<style scoped>
+</style>
