2a8d331ea599ce428fbb8284a55a3d5602ce6587
Manas Malla <manasmalla.dev@gmail.com>
2024-11-11 19:34:22 +0000

    Added benefits section and updated home stats
    Signed-off-by: Manas Malla <manasmalla.dev@gmail.com>

--- components/home/BenefitsSection.vue
+++ components/home/BenefitsSection.vue
@@ -4,20 +4,18 @@
         <p>We have something for everyone this DevFest, whether you are a budding entrepreneur or a passionate
             developer.<br />We got you all cover! Join us for the biggest bash of the year.</p>
         <v-row class="mt-4">
-            <v-expansion-panels variant="accordion">
-                <v-expansion-panel :style="index == 0 ? 'border-radius: 16px 16px 0px 0px;' : index == mainData.eventInfo.benefits.length - 1 ? 'border-radius: 0px 0px 16px 16px ;' : ''" v-for="(benefit, index) in mainData.eventInfo.benefits" :key="benefit.id">
-                    <v-expansion-panel-title v-slot="{ expanded }">
-                        <p v-if="!expanded">{{ benefit.title }}</p>
-                        <h3 v-if="expanded">{{ benefit.title }}</h3>
-                    </v-expansion-panel-title>
-                    <v-expansion-panel-text>
-                        <ul style="margin-left: 20px;">
-                            <li style="font-size: 14px;" v-for="point in benefit.benefits" :key="point">{{ point }}</li>
-                        </ul>
-                    </v-expansion-panel-text>
 
-                    </v-expansion-panel>
-            </v-expansion-panels>
+            <v-col v-for="(benefit, index) in mainData.eventInfo.benefits" :key="benefit.title" cols="12" md="6">
+                <v-container :style="(index <= 1 ? 'height: 210px;' : 'height: 250px; ') + 'padding: 0px; border-radius: 12px; border: 1.5px solid ' + benefit.color + '; background-color: ' + benefit.bgcolor " >
+                    <div
+                        :style="'margin: 12px 0px 0px 0px; width: fit-content; padding: 12px 24px !important; border-radius: 8px; overflow: hidden; color: ' + benefit.color">
+                        <h2>{{ benefit.title }}</h2>
+                    </div>
+                    <ul style="margin-left: 12px; padding: 8px 24px;">
+                        <li v-for="point in benefit.benefits" style="font-size: 14px;" :key="point">{{ point }}</li>
+                    </ul>
+                </v-container>
+            </v-col>
         </v-row>
     </v-container>
 </template>
