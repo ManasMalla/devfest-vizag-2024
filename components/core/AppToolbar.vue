407990aaa2e9684f7089de80ae0d224ee5553089
Manas Malla <manasmalla.dev@gmail.com>
2024-11-11 11:29:08 +0000

    Fixed flex for announcements. Disabled Sponsors section for now.
    Signed-off-by: Manas Malla <manasmalla.dev@gmail.com>

--- components/core/AppToolbar.vue
+++ components/core/AppToolbar.vue
@@ -78,7 +78,7 @@
     <div style="max-width:1000px; height: 72px; display: flex; align-items: center; border-radius: 0px 0px 24px 24px; margin-left: auto; margin-right:auto; left:0; right:0; background-color: #c3ecf6; padding: 32px 12px 12px 12px; position: fixed; top: 60px;z-index: 1005;">
       <p class="mr-3 ml-1" style="width: fit-content; display:flex; column-gap: 6px; align-items: center;"><b>Announcements</b> <v-icon size="18px">mdi-bullhorn-variant-outline</v-icon></p>
       <NuxtMarquee>
-       <div v-for="announcement in announcements"><p class="mx-3">{{announcement.text}}<a v-if="announcement.action != undefined" :href="announcement.action.link">{{announcement.action.text}}</a></p><p>•</p></div>
+       <div style="display: flex; align-items: center;" v-for="announcement in announcements"><p class="mx-3">{{announcement.text}}<a v-if="announcement.action != undefined" :href="announcement.action.link">{{announcement.action.text}}</a></p><p>•</p></div>
         </NuxtMarquee>
     </div></div>
   </div>
