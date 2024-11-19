21c001c2cc80234d916835a78ac61a67cfd0906d
MamathaGokarla <mamatha.gokarla@gmail.com>
2024-11-19 20:52:07 +0530

    sponsor design

--- components/common/sponsorInfo.vue
+++ components/common/sponsorInfo.vue
@@ -1,31 +1,37 @@
 <template>
   <v-container fluid class="pa-0 ma-0">
-   <v-row
-  v-for="(item, index) in sponsorsData"
-  :key="index"
-  class="google-font mb-5 mt-0"
-  style="display: flex; justify-content: center; align-items: center;"
->
-      <v-col md="12" cols="12" class="mb-n1"
-        ><b>{{ item.category_name }}</b></v-col
-      >
+    <v-row class="google-font mb-5 mt-0">
       <v-col
-        md="2"
-        cols="6"
-        sm="3"
-        class="text-center"
-        v-for="(sponsor, indexp) in item.sponsors"
-        :key="indexp"
-      >
+        v-for="(item, index) in sponsorsData"
+        :key="index"
+        cols="4" <!-- Ensures 3 columns per row for categories -->
+      
+        <div class="mb-3">
+          <b>{{ item.category_name }}</b>
+        </div>
         <div
-          style="background-color: #F3F3F3; border-radius: 15px;border: 1.5px solid black;"
-          class="pa-5"
+          v-for="(sponsor, indexp) in item.sponsors"
+          :key="indexp"
+          style="background-color: #F3F3F3; border-radius: 15px; border: 1.5px solid black; width: 200px;  height: 150px;"
+          class="pa-5 mb-3"
         >
           <ClientOnly>
             <v-tooltip location="bottom" :key="indexp">
               <template v-slot:activator="{ props }">
-                <a aria-label="sponsor name" :href="sponsor.link" target="_blank" v-bind="props">
-                  <v-img alt="sponsor-logo" :src="'/img/sponsors/' + sponsor.logo"></v-img>
+                <a
+                  aria-label="sponsor name"
+                  :href="sponsor.link"
+                  target="_blank"
+                  v-bind="props"
+                  style="display:block; width:100%; height:100%;"
+                >
+                  <v-img
+                  style="display:block; width:100%; height:100%;"
+                    alt="sponsor-logo"
+                    :src="'/img/sponsors/' + sponsor.logo"
+                    max-height="100"
+                    max-width="150"
+                  ></v-img>
                 </a>
               </template>
               <span>{{ sponsor.name }}</span>
