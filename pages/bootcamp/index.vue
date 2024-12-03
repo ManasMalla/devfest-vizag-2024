483d424a53d8a2539df34e033d012c0558ebb236
Manas Malla <manasmalla.dev@gmail.com>
2024-12-03 19:31:00 +0530

    changed url for bootcamp
    Signed-off-by: Manas Malla <manasmalla.dev@gmail.com>

--- pages/bootcamp/index.vue
+++ pages/bootcamp/index.vue
@@ -1,53 +1,51 @@
 <template>
-  <NuxtLayout name="default">
-    <v-container fluid class="mt-5">
-      <v-row>
-        <v-col md="12">
-          <h1>Bootcamp</h1>
-          <p>
-            The gateway to enter in another realm.
-          </p>
-        </v-col>
-      </v-row>
+    <NuxtLayout name="default">
+        <v-container fluid class="mt-5">
+            <v-row>
+                <v-col md="8">
+                    <h1>Bootcamp</h1>
+                    <p>The gateway to enter in another realm. Find it below!</p>
 
-      <v-row>
-        <v-col id="PSCard" md="6" cols="12" sm="6" lg="6" v-for="(item, index) in bootcampData" :key="index"
-          style="border-radius: 24px; gap: 20px; cursor: pointer;">
-          <!-- PS card -->
-          <v-card class="ma-2" style="border-radius: 16px;">
-            <v-card-title class="primary white--text"
-              style="background: #417ce0; color: white; text-align: center; padding: 50px; font-size: large; font-weight: 600;">
-              {{ item.title }}
-            </v-card-title>
-            <v-card-text style="padding: 0;">
-              <p class="mt-5" style="font-size: small; text-align: center; padding: 0 1rem;">{{ item.description }}</p>
-              <br>
-              <div style="width: 30%; height: 2px; border-radius: 10px; background-color: #417ce0; margin: 0 auto;">
-              </div>
-              <br>
-              <p
-                :style="{ width: 'fit-content', fontSize: 'smaller', fontWeight: 'bold', borderRadius: '32px', border: '1px solid', borderColor: CategoryColourCodes[item.category] || '#417ce0', padding: '10px 20px', color: CategoryColourCodes[item.category], textAlign: 'center', margin: '0 auto' }">
-                {{ item.category }}
-              </p>
-              <!-- <p>Price: ${{ item.price }}</p> -->
-              <div class="mt-5" style="background-color: #e7ac12; display: flex; justify-content: center; align-items: center; flex-direction: column; padding: 30px 0;">
-                <p style="text-align: center; font-size: smaller;">Powered by</p>
-                <p class=""
-                  style="text-align: center; color: white; font-weight: 600; width: 100%; font-size: medium; display: flex; justify-content: center; align-items: center;">
-                  <img :src="'/img/sponsors/msi.png'" alt="google logo" style="width: 80px;">
-                  {{ item.provider }}
-                </p>
-              </div>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </NuxtLayout>
+                    <v-expansion-panels class="mb-6 mt-10" rounded="12" bg-color="white" flat style="
+                border-radius: 20px !important;
+                /* border-bottom: 1px solid black; */
+                overflow: hidden;
+              " variant="accordion">
+                        <!-- :style="{ borderBottom: i<3?'1px solid black':'', borderTop: i!=0?'1px solid black':'' }" -->
+                        <v-expansion-panel v-for="(item, index) in bootcampData" :key="index" :style="{
+                            borderBottom:
+                                index + 1 < bootcampData.length ? '1px solid black' : '',
+                        }">
+                            <v-expansion-panel-title expand-icon="mdi-menu-down" style="background-color: #eeeeee">
+                                {{ item.title }}
+                            </v-expansion-panel-title>
+                            <v-expansion-panel-text class="pa-3">
+                                <div v-html="item.description"></div>
+
+                                <v-chip
+                                    :color="CategoryColourCodes[item.category]"
+                                    variant="outlined" class="mt-2">{{
+                                        item.category
+                                    }}</v-chip>
+                            </v-expansion-panel-text>
+                        </v-expansion-panel>
+                    </v-expansion-panels>
+                </v-col>
+                <!-- <div style="height: 100%;">
+                    <v-img alt="frame" class="frame" src="/img/bootcamp/bulb.png" style="height: 200px;"></v-img>
+                </div> -->
+            </v-row>
+
+        </v-container>
+    </NuxtLayout>
 </template>
 
 <script setup>
 const { mainData, bootcampData } = useJSONData();
+
+definePageMeta({
+    layout: false,
+});
 
 const CategoryColourCodes = {
   "Machine Learning": "#4285F4",
@@ -62,39 +60,26 @@ const CategoryColourCodes = {
   "Big Data": "#9CCC65"
 };
 
-definePageMeta({
-  layout: false,
-});
-
 useSeoMeta({
-  contentType: "text/html; charset=utf-8",
-  title:
-    "Speakers - " + mainData.eventInfo.name + " | " + mainData.communityName,
-  description: mainData.eventInfo.description.short,
-  keywords: mainData.seo.keywords,
-  ogLocale: 'en_US',
-  author: "OSS Labs",
-  creator: "OSS Labs",
-  viewport: "width=device-width, initial-scale=1.0",
-  ogTitle:
-    "Speakers - " + mainData.eventInfo.name + " | " + mainData.communityName,
-  ogDescription: mainData.eventInfo.description.short,
-  ogImage: `${mainData.seo.hostUrl}/thumbnail.png?auto=format&fit=crop&frame=1&h=512&w=1024`,
-  ogUrl: mainData.seo.hostUrl,
-  ogType: "website",
-  twitterTitle:
-    "Speakers - " + mainData.eventInfo.name + " | " + mainData.communityName,
-  twitterDescription: mainData.eventInfo.description.short,
-  twitterImage: `${mainData.seo.hostUrl}thumbnail.png?auto=format&fit=crop&frame=1&h=512&w=1024`,
-  twitterCard: "summary_large_image",
+    contentType: "text/html; charset=utf-8",
+    title: "Bootcamp - " + mainData.eventInfo.name + " | " + mainData.communityName,
+    description: mainData.eventInfo.description.short,
+    keywords: mainData.seo.keywords,
+    ogLocale: 'en_US',
+    author: "OSS Labs",
+    creator: "OSS Labs",
+    viewport: "width=device-width, initial-scale=1.0",
+    ogTitle: "Bootcamp - " + mainData.eventInfo.name + " | " + mainData.communityName,
+    ogDescription: mainData.eventInfo.description.short,
+    ogImage: `${mainData.seo.hostUrl}/thumbnail.png?auto=format&fit=crop&frame=1&h=512&w=1024`,
+    ogUrl: mainData.seo.hostUrl,
+    ogType: "website",
+    twitterTitle:
+        "Bootcamp - " + mainData.eventInfo.name + " | " + mainData.communityName,
+    twitterDescription: mainData.eventInfo.description.short,
+    twitterImage: `${mainData.seo.hostUrl}thumbnail.png?auto=format&fit=crop&frame=1&h=512&w=1024`,
+    twitterCard: "summary_large_image",
 });
 </script>
 
-<style scoped>
-#PSCard:hover {
-  transition: all;
-  transform: scale(1.05);
-  transition-duration: 250ms;
-  z-index: 10;
-}
-</style>
+<style scoped></style>
