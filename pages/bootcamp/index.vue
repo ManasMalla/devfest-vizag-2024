636c06584e14c3c8a3a04f8a4c24d8673f325b8b
ChandanKhamitkar <khamitkarsaichandan1035@gmail.com>
2024-12-03 20:31:18 +0530

    Added Apply functionality to bootcamp PS
    Signed-off-by: ChandanKhamitkar <khamitkarsaichandan1035@gmail.com>

--- pages/bootcamp/index.vue
+++ pages/bootcamp/index.vue
@@ -5,6 +5,9 @@
                 <v-col md="8">
                     <h1>Bootcamp</h1>
                     <p>The gateway to enter in another realm. Find it below!</p>
+                    <v-btn v-if="appliedPS !== -1" class="mt-2"variant="flat" fluid :color="'#ea4335'" v-bind:href="'mailto:vizaggdg@gmail.com?subject=Request for Change in Problem Statement&body=I want to change my Problem Statement to ...'" >
+                        CHANGE PS
+                    </v-btn>
 
                     <v-expansion-panels class="mb-6 mt-10" rounded="12" bg-color="white" flat style="
                 border-radius: 20px !important;
@@ -22,11 +25,21 @@
                             <v-expansion-panel-text class="pa-3">
                                 <div v-html="item.description"></div>
 
-                                <v-chip
-                                    :color="CategoryColourCodes[item.category]"
-                                    variant="outlined" class="mt-2">{{
-                                        item.category
-                                    }}</v-chip>
+                                <div style="display: flex; justify-content: start; align-items: center; gap: 10px;">
+                                    <v-chip :color="CategoryColourCodes[item.category]" variant="outlined"
+                                        class="mt-2">{{
+                                            item.category
+                                        }}</v-chip>
+
+                                    <v-btn v-if="appliedPS === -1 || appliedPS === item.id" :loading="isLoading"
+                                        @click="applyBootCamp(item.id)" variant="text"
+                                        :color="appliedPS === item.id ? '#34a853' : '#4285f4'" class="mt-2">
+                                        <span style="font-weight: 600;">{{ appliedPS === item.id ? 'Applied' :
+                                            'Apply' }}</span>
+                                        <v-icon class="ml-2">{{ appliedPS === item.id ? "mdi-check" :
+                                            "mdi-plus-circle" }}</v-icon>
+                                    </v-btn>
+                                </div>
                             </v-expansion-panel-text>
                         </v-expansion-panel>
                     </v-expansion-panels>
@@ -41,23 +54,64 @@
 </template>
 
 <script setup>
+import { addDoc, collection, getCountFromServer, getDocs, limit, query, where } from 'firebase/firestore';
 const { mainData, bootcampData } = useJSONData();
+const user = useCurrentUser();
+const db = useFirestore();
+const isLoading = ref(false);
+const appliedPS = useState('appliedPS', () => -1);
 
 definePageMeta({
     layout: false,
 });
 
+watch(user, (_) => {
+    if (user.value) {
+        fetchAppliedPs();
+    }
+});
+
+async function fetchAppliedPs() {
+    try {
+        const q = query(collection(db, 'bootcamp'), where("uid", "==", user.value.uid));
+        const snapshot = await getDocs(q);
+        snapshot.forEach((doc) => {
+            appliedPS.value = doc.data().psid;
+        });
+    } catch (error) {
+        console.error("Error in fetching applied Bootcamps : ", error);
+    }
+}
+
+
+async function applyBootCamp(psID) {
+    try {
+        isLoading.value = true;
+        if (user.value) {
+            await addDoc(collection(db, "bootcamp"), {
+                uid: user.value.uid,
+                psid: psID
+            });
+            isLoading.value = false;
+            alert("Applied Successfully ✅");
+        }
+    } catch (error) {
+        console.error("Error in applying... : ", error);
+        isLoading.value = false;
+    }
+}
+
 const CategoryColourCodes = {
-  "Machine Learning": "#4285F4",
-  "Natural Language Processing": "#EA4335",
-  "Computer Vision": "#FBBC05",
-  "Recommender Systems": "#34A853",
-  "Predictive Analytics": "#00897B",
-  "Data Analysis": "#673AB7",
-  "Deep Learning": "#0F9D58",
-  "Cloud Computing": "#42A5F5",
-  "Data Mining": "#F48FB1",
-  "Big Data": "#9CCC65"
+    "Machine Learning": "#4285F4",
+    "Natural Language Processing": "#EA4335",
+    "Computer Vision": "#FBBC05",
+    "Recommender Systems": "#34A853",
+    "Predictive Analytics": "#00897B",
+    "Data Analysis": "#673AB7",
+    "Deep Learning": "#0F9D58",
+    "Cloud Computing": "#42A5F5",
+    "Data Mining": "#F48FB1",
+    "Big Data": "#9CCC65"
 };
 
 useSeoMeta({
