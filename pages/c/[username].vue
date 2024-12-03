fbcf73c2f584d63ce6597a185b41fabe2b7b7493
ChandanKhamitkar <khamitkarsaichandan1035@gmail.com>
2024-12-04 01:08:00 +0530

    Added funitonality to network
    Signed-off-by: ChandanKhamitkar <khamitkarsaichandan1035@gmail.com>

--- pages/c/[username].vue
+++ pages/c/[username].vue
@@ -1,4 +1,5 @@
 <template>
+    <NuxtLoadingIndicator :color="'#4285f4'"/>
     <h1 class="mt-5">Learn. Connect. Grow.</h1>
     <div style="width: 90vw; max-width: 1200px;">
         <v-row>
@@ -7,51 +8,148 @@
                     style="padding: 12px; border-radius: 24px; margin: 12px; max-height: 500px; margin-left: auto; margin-right: auto; position: relative; display: flex; flex-direction: column;"
                     elevation="0" :color="userDomain == 'Mobile' ? '#CEE6C1' : '#c3ecf6'">
                     <img src="/img/attendee/mobile.png" height="120px" style="position: absolute;" />
-                    <img src="https://github.com/ManasMalla.png"
+                    <!-- Headshot -->
+                    <img :src="peerDetails?.photoURL"
                         style="height: 240px; width: 100%; object-fit: cover; border-radius: 14px; object-position: top;" />
                     <div class="card-inner-div" style="width: 100%; margin-top: 12px; display: flex; ">
                         <div style="width: 100%; flex-grow: 1;">
-                            <h2 style="line-height: 100%; margin-top: 12px;">Manas Malla</h2>
+                            <!-- Display Name -->
+                            <div style="display: flex; justify-content: start; align-items: center; gap: 8px;">
+                                <h2 style="line-height: 100%; margin-top: 12px;">{{ peerDetails?.displayName }}</h2>
+                                <v-icon class="mt-3" v-if="isAlreadyNetworked" :color="'#34a853'">mdi-check-decagram</v-icon>
+                            </div>
                             <p style="opacity: 50%; font-weight: 600; margin-bottom: 6px;">@{{ $route.params.username }}
                             </p>
-                            <p style="font-size: 14px;">Founder, The Ananta Studio</p>
+                            <p style="font-size: 14px;">{{ peerDetails?.company?.designation }} , {{
+                                peerDetails?.company?.name }}</p>
                             <v-row class="my-2 pl-3" style="column-gap: 12px; flex-grow: 0;">
-                                <v-icon>mdi-linkedin</v-icon>
-                                <v-icon>mdi-github</v-icon>
-                                <v-icon>mdi-web</v-icon>
+                                <div v-for="social in peerDetails?.socials" :key="social.name">
+                                    <a :href="social.provider === 'instagram' ? `https://www.instagram.com/${social.name}`
+                                        : social.provider === 'github' ? `https://github.com/${social.name}`
+                                            : social.provider === 'linkedin' ? `https://www.linkedin.com/in/${social.name}`
+                                                : ''" target="_blank" style="color: black;">
+                                        <v-icon>{{ social.icon }}</v-icon>
+                                    </a>
+                                </div>
                             </v-row>
                         </div>
                         <div class="my-2" style="flex-grow: 1;">
-                            <v-chip
-                                style="width: fit-content; font-size: 12px; display: inline-flex; margin-right: 4px; margin-top: 8px;">#WebAssembly</v-chip>
-                            <v-chip
-                                style="width: fit-content; font-size: 12px; display: inline-flex; margin-right: 4px; margin-top: 8px;">#Android</v-chip>
-                            <v-chip
-                                style="width: fit-content; font-size: 12px; display: inline-flex; margin-right: 4px; margin-top: 8px;">#Kotlin</v-chip>
+                            <v-chip v-for="domain in peerDetails?.domainsInterested"
+                                style="width: fit-content; font-size: 12px; display: inline-flex; margin-right: 4px; margin-top: 8px;">#{{ domain }}</v-chip>
                         </div>
                         <p style="position: absolute; right: 16px; bottom: 12px; opacity: 0.4; font-weight: 600">
-                            #Web</p>
+                            #{{ peerDetails?.domainsInterested[0] }}</p>
                     </div>
                 </v-card>
             </v-col>
-            <v-col cols="12" lg="8">
+            <v-col v-if="!isAlreadyNetworked" cols="12" lg="8">
                 <h3>What was the convo about?</h3>
                 <div class="convo-stack" style="column-gap: 12px; row-gap: 12px; margin-top: 24px;">
                     <div class="tech-stack" v-for="techStack in tracks"
-                        style="display: inline-flex; width: fit-content; align-items: center; column-gap: 12px;">
-                        <input type="checkbox">
-                        <p style="display: inline-flex; width: fit-content;">{{ techStack.title }}</p>
-                        <!-- <div class="mobile-img"></div>
+                        style="display: inline-flex; width: fit-content; align-items: center; column-gap: 12px;"
+                        :key="techStack.title">
+                        <!-- <input type="checkbox">
+                        <p style="display: inline-flex; width: fit-content;">{{ techStack.title }}</p> -->
+                        <v-checkbox :label="techStack.title" color="primary" v-model="choosenTechStacks"
+                            :value="techStack.title" /> <!-- <div class="mobile-img"></div>
                         <h3 class="mt-2">{{ techStack.title }}</h3>
                         <p>{{ techStack.desc }}</p> -->
                     </div>
                 </div>
+                <v-btn @click="createConnection" :loading="isSumbitLoading" v-if="choosenTechStacks.length !== 0"
+                    variant="tonal" style="float: right">Submit</v-btn>
             </v-col>
         </v-row>
     </div>
 
 </template>
 <script setup>
+import { collection, addDoc, query, getDocs, where, and, or, getCountFromServer } from 'firebase/firestore';
+
+
+const user = useCurrentUser();
+const db = useFirestore();
+const route = useRoute();
+
+const isPageLoading = ref(false);
+const isSumbitLoading = ref(false);
+const peerDetails = useState('peerDetails', () => { });
+const choosenTechStacks = ref([]);
+const isAlreadyNetworked = ref(false);
+
+const peerId = ref('');
+const targetId = ref('');
+
+watch(user, (_) => {
+    isPageLoading.value = true;
+    if (user.value) {
+        fetchPeersData();
+    }
+});
+// FETCH Peer's data
+async function fetchPeersData() {
+    try {
+        console.log('Function called');
+        const peerUserName = route.params.username;
+        const q = query(collection(db, "users"), where("username", "==", peerUserName));
+        const snapshot = await getDocs(q);
+        snapshot.forEach((doc) => {
+            peerDetails.value = doc.data();
+            peerId.value = doc.id;
+            console.log('uid = ', doc.id);
+        });
+        checkNetworked();
+    } catch (error) {
+        console.error('Error in fetching Peer. :', error);
+        alert('Error in fetching Peer, Please try again!!');
+    }
+}
+
+// check if already Networked 
+async function checkNetworked() {
+    try {
+        targetId.value = user?.value.uid;
+        console.log('Peer id = ', peerId.value, " target id = ", targetId.value);
+        const q = query(
+            collection(db, "connections"),
+            or(
+                and(
+                    where("connectA", "==", peerId.value),
+                    where("connectB", "==", targetId.value)
+                ),
+                and(
+                    where("connectA", "==", targetId.value),
+                    where("connectB", "==", peerId.value)
+                )
+            )
+        );
+        const snapshot = await getCountFromServer(q);
+        const count = snapshot.data().count;
+        isAlreadyNetworked.value = count !== 0 ? true : false; 
+    } catch (error) {
+        console.error("Error in Checking Already Networked or not! = ", error);
+    }
+}
+
+// AddDoc to Connections
+async function createConnection() {
+    isSumbitLoading.value = true;
+    try {
+        await addDoc(collection(db, "connections"), {
+            connectA: user.value.uid,
+            connectB: peerId.value,
+            domains : choosenTechStacks.value
+        });
+
+        alert('Updated Successfully ✅');
+    } catch (error) {
+        console.error("Error in Creating doc in connection collection : ->", error);
+        alert("Error in UPDATING network.")
+        isSumbitLoading.value = false;
+    }
+}
+
+
 const tracks = [{
     title: 'Mobile',
     img: 'https://io.google/2024/app/images/io24-stacks-m.webp',
@@ -95,11 +193,11 @@ const userDomain = 'Mobile';
     padding: 12px;
 }
 
-.tech-stack:hover {
+/* .tech-stack:hover {
     border: 1.5px solid #202023;
     width: fit-content;
     border-radius: 20px;
-}
+} */
 
 .badge-card .card-inner-div {
     flex-direction: row;
