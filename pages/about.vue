3fef7e2118a0f60ec7137f6f36fa7ac74ec0027f
Manas Malla <manasmalla.dev@gmail.com>
2024-12-06 00:55:51 +0530

    added about page
    Signed-off-by: Manas Malla <manasmalla.dev@gmail.com>

---
+++ pages/about.vue
@@ -0,0 +1,61 @@
+<template>
+    <h1 class="mt-5">Behind the Experience</h1>
+    <v-img :src="'/img/about/manas-varshita.jpg'" />
+    <div style="display: flex; column-gap: 48px; justify-content: space-between; margin-top: 12px;">
+        <p style="max-width: 450px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id consectetur nibh.
+            Pellentesque nec nibh ut arcu hendrerit faucibus interdum ut ex. Mauris ultrices felis in erat rhoncus
+            pellentesque. Phasellus nec bibendum nunc, a euismod purus. Vivamus sed nibh ligula. Maecenas placerat at
+            eros
+            vitae pharetra. Quisque elit turpis, scelerisque nec eleifend vel, faucibus eu sapien. Praesent eget
+            ullamcorper
+            felis. Sed dignissim ullamcorper risus ut facilisis. Aenean quis consectetur massa. Donec rhoncus lorem a
+            sapien
+            dictum fermentum. Praesent at maximus eros, at accumsan massa. Sed mattis lobortis tortor viverra mollis.
+            <br /><br />
+            Pellentesque ut augue blandit, pharetra nibh id, convallis libero. Sed elit elit, suscipit vel bibendum
+            eget,
+            aliquam a erat. Duis gravida leo sit amet egestas tincidunt. Nulla blandit tempus elit mattis aliquam. Proin
+            purus sem, elementum a suscipit eget, venenatis sit amet leo. Quisque lacinia vulputate massa, gravida
+            varius
+            ipsum gravida vitae. Cras sit amet ipsum faucibus, efficitur turpis quis, volutpat lorem. Nam at fringilla
+            nisl.
+            Curabitur suscipit mi sit amet imperdiet vulputate. In nec bibendum nisl, sed dictum metus.
+            <br /><br />
+            Etiam mauris tortor, tincidunt id nisl a, dictum fringilla lacus. Integer finibus ac nisl et feugiat.
+            Suspendisse vitae rutrum lacus. Fusce at consectetur est. Fusce convallis orci vitae mauris aliquam
+            pharetra.
+            Nunc maximus est sed neque facilisis mattis. Nunc nec consequat felis. Vivamus varius quam lectus, ac
+            hendrerit
+            nunc faucibus eget. Quisque et aliquam erat. Cras scelerisque, sapien vel pharetra eleifend, turpis diam
+            maximus
+            leo, sed scelerisque risus urna quis purus. Vivamus aliquam consequat massa ut tempus. In at placerat nulla.
+            Integer ultrices risus quis sem vulputate, at faucibus sem dapibus.
+            <br /><br />
+
+        </p>
+        <p style="max-width: 450px;">Maecenas non nisi erat. Curabitur eu tempus sapien, at egestas ipsum. Sed et
+            fringilla massa. Nunc ac
+            eleifend
+            massa, a commodo urna. Mauris nulla metus, lobortis eu lectus id, viverra interdum lorem. Proin tortor
+            velit,
+            luctus ut tellus ac, volutpat eleifend nulla. Pellentesque placerat pretium quam in euismod. Pellentesque
+            sed
+            auctor eros, ac feugiat orci. Aenean nulla neque, efficitur non condimentum id, fermentum a urna. Curabitur
+            mattis enim lorem, maximus dapibus lacus ullamcorper vitae.
+            <br /><br />
+            Curabitur gravida facilisis lectus. Duis congue sed urna eleifend ultricies. Suspendisse interdum dolor et
+            tempus interdum. In efficitur, velit ac dapibus tincidunt, risus massa porttitor massa, non viverra mauris
+            nibh
+            eu risus. Morbi aliquet purus eros. Morbi id nibh laoreet, scelerisque neque eu, porta lectus. Curabitur ut
+            odio
+            eros. Fusce at ultrices urna. Morbi pulvinar urna ex, a malesuada tortor ultrices vitae. In sed eros non
+            tellus
+            vulputate consectetur vel lobortis dolor. Phasellus et fermentum orci, eget tincidunt dui. Interdum et
+            malesuada
+            fames ac ante ipsum primis in faucibus. Vivamus ante lorem, tincidunt ut metus eget, lobortis pellentesque
+            mauris. Aenean blandit posuere libero vel lacinia. Maecenas et ultrices felis, nec laoreet ipsum.
+            Pellentesque
+            vitae metus felis.
+        </p>
+    </div>
+</template>
